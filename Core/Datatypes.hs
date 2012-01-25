9adee113aa66ebd6fbd5a585f3051490e09cc21f
Paul van der Walt <paul@denknerd.org>
2012-01-25 17:17:23 +0100

    data types for config file. subject to change

--- Core/Datatypes.hs
+++ Core/Datatypes.hs
@@ -16,3 +16,17 @@ instance Eq DotFS where
 
 data Conf = C FilePath deriving Show
 
+
+data ConfigFile = Vanilla String
+                | Special Header Body
+                deriving Show
+
+type Body       = [Block]
+type Header = String
+type Condition  = String
+type VariableName  = String
+
+data Block    = FreeText String
+              | Conditional Condition Block
+              | Variable VariableName
+              deriving Show
