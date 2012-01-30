252ff500e26e5c4bf1de6972d6a68e3da34e55a9
Sjoerd Timmer <sjoerd.timmer@gmail.com>
2012-01-30 23:35:09 +0100

    the most awesome, extermely fancy datatypes, no parser yet whatsoever...

--- Core/Datatypes.hs
+++ Core/Datatypes.hs
@@ -1,3 +1,4 @@
+{-#LANGUAGE GADTs, EmptyDataDecls, KindSignatures, ExistentialQuantification #-}
 module Core.Datatypes where
 
 import System.Fuse
@@ -13,4 +14,58 @@ data DotFS = DotFS {
 
 instance Eq DotFS where
   (==) x y = dotfsEntryName x == dotfsEntryName y
+
+
+-- Types for parsed stuff
+type Var    = String
+type Header = [Assignment]
+
+data Assignment = forall a. Show a => Assign Var (Expr a)
+                | Execute Var String
+                | TagStyle String String
+                | CommentStyle String String
+
+instance Show Assignment where
+    show (Assign v e)         = (show v)++" <- "++(show e)
+    show (Execute v c)        = (show v)++" <= "++(show c)
+    show (TagStyle s1 s2)     = s1++"tag"++s2
+    show (CommentStyle s1 s2) = s1++"comment"++s2
+
+data Expr a where
+         -- constants
+         Int    :: Int -> Expr Int
+         Bool   :: Bool -> Expr Bool
+         String :: String -> Expr String
+         -- int operators
+         Add    :: Expr Int -> Expr Int -> Expr Int
+    --     Sub    :: Expr Int -> Expr Int -> Expr Int
+--         Mul    :: Expr Int -> Expr Int -> Expr Int
+--         Div    :: Expr Int -> Expr Int -> Expr Int
+         -- string operators
+--         Con    :: Expr String -> Expr String -> Expr String
+         -- comparators
+         Eq     :: Expr a -> Expr a -> Expr Bool
+--         Neq    :: Expr a -> Expr a -> Expr Bool
+         Gt     :: Expr Int -> Expr Int -> Expr Bool
+         -- boolean operators
+         And    :: Expr Bool -> Expr Bool -> Expr Bool
+--         Or     :: Expr Bool -> Expr Bool -> Expr Bool
+--       -- conditonal
+         If     :: Expr Bool -> Expr a -> Expr a -> Expr a
+
+instance Show (Expr a) where
+    show (Int i)     = show i
+    show (Bool b)    = show b
+    show (String s)  = show s
+    show (Add i1 i2) = (show i1)++"+"++(show i2)
+    show (Eq e1 e2)  = (show e1)++"+"++(show e2)
+    show (Gt i1 i2)  = (show i1)++">"++(show i2)
+    show (And b1 b2) = (show b1)++"&&"++(show b2)
+    show (If c i e)  = "if("++(show c)++"){"++(show i)++"}{"++(show e)++"}"
+
+
+
+
+
+
 
--- Core/Parser.hs
+++ Core/Parser.hs
@@ -1,16 +1,33 @@
 module Core.Parser where
 
 import Core.Datatypes
-import Core.PrettyPrinter
 
-import Control.Applicative ((<*),(<$>),(<*>),(*>))
+import Control.Applicative ((<*),(<$>),(<*>),(*>),(<$))
+import Control.Monad (join)
 import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Char
 import Text.Parsec.Error
-import Text.Parsec.Token
+import Text.Parsec.Token as P
 import Text.Parsec.Prim
 import Text.Parsec.Language
+import Text.Parsec.Expr
+
+-- stuff about the language and the default lexer
+lang :: LanguageDef st
+lang = javaStyle
+     { reservedNames = ["commentstyle","tagstyle","if","else"]
+     , opStart = opLetter headerLang
+     , opLetter = oneOf "~!@#$%^&*()_+|`-=\\{}[]:;'<>?,./" -- only used to parse tagsdefs
+     , reservedOpNames = [""]                -- things that cannot be the tags-delimiters
+     , caseSensitive = True
+     }
+
+tp = P.makeTokenParser lang
+
+-- first pass:
+
+headerP :: Parser Header
 
 
 
