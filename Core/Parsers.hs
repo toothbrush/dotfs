9c78d59a685e39dcb59422ac82c65a1ab35f91b2
Paul van der Walt <paul@denknerd.org>
2012-02-02 23:47:07 +0100

    start of header evaluation

--- Core/Parsers.hs
+++ Core/Parsers.hs
@@ -5,6 +5,7 @@ import Core.HeaderParser (dotfsP)
 import Core.HelperParsers
 import Core.Lexers
 import Core.ExpressionParsers
+import Core.ExpressionEvaluator
 
 import Control.Applicative ((<*),(<$>),(<*>),(*>),(<$))
 import Text.Parsec
@@ -25,22 +26,26 @@ splitter = try ( (\a b c->(b,a++c)) <$> manyTill anyChar (lookAhead (string "<<d
 
 
 -- test the parsing on a given file
+testfile :: FilePath -> IO ()
 testfile name = do fc <- readFile name
-                   return $ process fc
+                   putStrLn $ process fc
 
 -- run the header parser and evauator, and then the body parser on the result
 process :: String -> String
 process inp = case parse splitter "split" inp of
               Left err -> "error = \n" ++show (errorPos err) ++ "\n"
-              Right (head,body) -> show (head,body)
+              -- TODO: use the result of evaluate head (which can tell us commentStyle etc
+              -- to parse the body.
+              Right (head,body) -> show (evaluate head,body)
 
 
 
 evaluate :: Header -> VarList
-evaluate (TagStyle _ _:rest) = evaluate rest
-evaluate (CommentStyle _ _:rest) = evaluate rest
-evaluate (Execute name cmd:rest) =  undefined
-evaluate (Assign name val:rest) = undefined
+evaluate []                       = []
+evaluate (TagStyle _ _     :rest) = evaluate rest
+evaluate (CommentStyle _ _ :rest) = evaluate rest
+evaluate (Execute name cmd :rest) = (name, StringVal cmd):evaluate rest -- todo: system call
+evaluate (Assign name val  :rest) = (name, eval val) : evaluate rest
 
 
 
