43c9281c043b02844562b407983dfc2ea911db7d
Paul van der Walt <paul@denknerd.org>
2012-03-16 15:53:58 +0000

    starting with the evaluator

--- Core/ExpressionEvaluator.hs
+++ Core/ExpressionEvaluator.hs
@@ -4,7 +4,13 @@
 module Core.ExpressionEvaluator where
 
 import Core.Datatypes
+import Data.Maybe
+import Data.Map
 
-eval :: DFSExpr -> Value
-eval (Int i) = IntVal i
-eval _ = undefined
+eval :: DFSState -> DFSExpr -> Value
+eval s (Prim p) = p
+eval s (Var v)  = eval s $ s!v
+eval s (Sys c)  = VString ("to be executed: " ++ c)
+eval s (If c t e) = case eval s c of
+                        VBool True -> eval s t
+                        _          -> eval s e
