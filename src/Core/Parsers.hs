16944c08043a24a1229389c71c53ded9dbcee4a7
Paul van der Walt <paul@denknerd.org>
2012-03-20 22:47:41 +0100

    information about a hidden if-block

--- src/Core/Parsers.hs
+++ src/Core/Parsers.hs
@@ -43,18 +43,18 @@ fileP f = try (do { whiteSpace lex
 -- run the header parser and evaluator, and then the body parser on the result
 process :: FilePath -> ByteString -> ByteString
 process file contents =
-              let inp = unpack contents in
-              case runParser (fileP False) empty file inp of
-              Left err -> pack$ "error = \n" ++ show (errorPos err) ++ "\n"
-              Right s  -> case s of
-                    Vanilla _     -> contents
-                    Annotated h b -> pack$ present h b
+                       let inp = unpack contents in
+                         case runParser (fileP True) empty file inp of
+                         Left err -> pack$ "error = \n" ++ show (errorPos err) ++ "\n"
+                         Right s  -> case s of
+                               Vanilla _     -> contents
+                               Annotated h b -> pack$ present h b
 
 present :: Header -> Body -> String
 present _ []     = ""
 present h (Cond c b:bs) = case eval h c of
                                 VBool True -> outputInfoIf h c ++ present h b ++ outputEndIf h c
-                                _          -> ""
+                                _          -> outputFalseIf h c
                             ++ present h bs
 present h (Ref r:bs)    = outputInfoRef h r ++ show (eval h r) ++ present h bs
 present h (Verb v:bs)   = v ++ present h bs
@@ -68,6 +68,15 @@ outputInfoRef h e        = case lookup "commentstart" h of
                                Just (Prim (VString stop)) -> unwords [start,"ref:",show e,stop]
                                _ -> ""
                              _ -> ""
+outputFalseIf :: Header -> DFSExpr -> String
+outputFalseIf h e       = case lookup "commentstart" h of
+                            Nothing -> ""
+                            Just (Prim (VString start)) ->
+                              case lookup "commentstop" h of
+                              Nothing -> concat ["\n",start," hiding block because: \n",start,"false == ",show e,"\n"]
+                              Just (Prim (VString stop)) -> unwords [start,"evals to false: ",show e,stop]
+                              _ -> ""
+                            _ -> ""
 outputInfoIf :: Header -> DFSExpr -> String
 outputInfoIf h e        = case lookup "commentstart" h of
                             Nothing -> ""
