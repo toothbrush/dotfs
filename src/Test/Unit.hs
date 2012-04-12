8881406db1dcc446626ddc89b9218c86f9cda56b
Paul van der Walt <paul@denknerd.org>
2012-04-12 19:56:52 +0200

    move instances here, get rid of main :: IO()

--- src/Test/Unit.hs
+++ src/Test/Unit.hs
@@ -1,15 +1,41 @@
 {-# OPTIONS_GHC -fno-warn-unused-imports -fforce-recomp -fth #-}
-module Main where
+module Test.Unit where
 
 import Test.Utility -- our TH functions
 import Test.Tests -- our test cases
 import Control.Monad
 import Test.QuickCheck
+import Core.Datatypes
 import Test.QuickCheck.Test
 import System.Exit
 
 runTests :: IO ()
 runTests = $(mkChecks tests)
 
-main :: IO ()
-main = runTests
+-- Arbitrary instances:
+
+-- there's actually a reason these instances are here, even if
+-- they seem like orphans. The TH above requires them, but ghc doesn't
+-- realise this at compile time.
+instance Arbitrary DFSExpr where
+    arbitrary  =  sized expr
+        where
+          expr n | n <= 0     =  atom
+                 | otherwise  =  oneof [ atom
+                                       , liftM2 UniOp unioperator subform
+                                       , liftM3 BiOp bioperator subform subform'
+                                       ]
+                 where
+                   atom = oneof [ liftM Var (elements ["P", "Q", "R", "S"])
+                                , liftM Prim arbitrary
+                                ]
+                   subform  = expr (n `div` 2)
+                   subform' = expr (n `div` 4)
+                   unioperator = elements [Not]
+                   bioperator  = elements [Add, Sub, Mul, Div, And, Or, Eq, LTOp, GTOp, GEQ, LEQ, NEQ]
+
+instance Arbitrary Value where
+    arbitrary = oneof [ liftM VBool   arbitrary
+                      , liftM VInt    arbitrary
+                      , liftM VString arbitraryStr
+                      ]
